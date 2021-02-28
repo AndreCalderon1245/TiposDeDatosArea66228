b16e3db412f7e7453a5c93cf98399899c8c44dc7
End User <End User@DESKTOP-62AKJQO>
2021-02-28 17:22:10 -0600

    Link Git

---
+++ src/tipodedatosarea66228/TipoDeDatosArea66228.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tipodedatosarea66228;
+
+/**
+ *
+ * @author End User
+ */
+public class TipoDeDatosArea66228 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double base = 10;
+        double altura = 5;
+        double area = ((base * altura) / 2);
+        System.out.print(area + "\n");
+    }
+    
+}
